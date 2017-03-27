a9541839d974ce95fbc49c87d347fdc9493a0b88
pritam-das <pritam.das@mail.utoronto.ca>
2017-03-27 15:18:44 -0400

    PizzaMaker assignment complete

---
+++ exercise5.rb
@@ -0,0 +1,11 @@
+#PizzsMaker assignment
+
+puts "Hi there! How many pizzas would you like today?"
+
+quantity = gets.chomp
+
+puts "And how many toppings would you like to add to your pizza?"
+
+quantity2 = gets.chomp
+
+puts "You have ordered #{quantity} pizzas with #{quantity2} toppings."
